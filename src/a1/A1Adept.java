a389ce98b88fb86e0fc8ea61895de85dee841935
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2019-08-31 11:53:00 -0400

    Adept done

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,14 +1,67 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int count = Integer.parseInt(scan.next());
+		Dictionary item_price = new Hashtable();
+		for (int i=0; i<count; i++) {
+			String food = scan.next();
+			float price = Float.parseFloat(scan.next());
+			item_price.put(food, price);
+		}
+		
+		int people = Integer.parseInt(scan.next());
+		String[] names = new String[people];
+		float[] groceries_costs = new float[people];
+		
+		
+		for (int j=0; j<people; j++) {
+			String first_name = scan.next();
+			String last_name = scan.next();
+			names[j] = first_name+" "+last_name;
+			int num_of_items = Integer.parseInt(scan.next());
+			float total = 0;
+			for (int k = 0; k<num_of_items; k++) {
+				int amount = Integer.parseInt(scan.next());
+				String food = scan.next();
+				Float price =  (Float) item_price.get(food);
+				total += amount*price;
+				groceries_costs[j] = total;
+			}
+		}
+		
+		scan.close();
+		float min = groceries_costs[0];
+		float max = groceries_costs[0];
+		int max_index = 0;
+		int min_index = 0;
+		String max_person = "";
+		String min_person = "";
+		float sum = 0;
+		for (int i=0; i<names.length; i++) {
+			
+			if(min>=groceries_costs[i]) {
+				min = groceries_costs[i];
+				min_person = names[i];
+			}
+			if(max<=groceries_costs[i]) {
+				max = groceries_costs[i];
+				max_person=names[i];
+			}
+			sum += groceries_costs[i];
+			
+			
+		}
+		System.out.println("Biggest:"+' '+max_person+ " ("+String.format("%.02f",max)+")");
+		System.out.println("Smallest:"+' '+min_person+ " ("+String.format("%.02f",min)+")");
+		System.out.println("Average: "+ String.format("%.02f", sum/groceries_costs.length));
+		
 		
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -12,7 +12,6 @@ public class A1Novice {
 
 		// Read in count of numbers to process
 		int count = Integer.parseInt(scan.next());
-		//String str1 = scan.nextLine();
 		String[] names = new String[count];
 		float[] groceries_costs = new float[count];
 		
@@ -20,8 +19,6 @@ public class A1Novice {
 			String first_name = scan.next();
 			String last_name = scan.next();
 			int num_of_items = Integer.parseInt(scan.next());
-			//String[] first_last_qty = name_qty.split(" ");
-			//int num_of_items = Integer.parseInt(first_last_qty[2]);
 			names[i] = first_name.charAt(0)+". "+last_name;
 			
 			float total = 0;
@@ -29,10 +26,6 @@ public class A1Novice {
 				int amount = Integer.parseInt(scan.next());
 				String food = scan.next();
 				float price = Float.parseFloat(scan.next());
-				//String[] amount_then_cost = amount_cost.split(" ");
-				
-				//int amount = Integer.parseInt(amount_then_cost[0]);
-				
 				total += amount*price;
 				groceries_costs[i] = total;
 			}
