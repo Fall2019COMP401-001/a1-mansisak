b0c7c77a772af5e20c7eaabbabad5c0bf5ea8fbd
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2019-08-31 14:45:05 -0400

    -Base is working: how many ppl bought, how many items bought

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,8 @@
 package a1;
 
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -8,7 +11,51 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int count = Integer.parseInt(scan.next());
+		Dictionary item_amount = new Hashtable();
+		Dictionary item_pplWhoBought = new Hashtable();
+		
+		for (int i=0; i<count; i++) {
+			String food = scan.next();
+			float price = Float.parseFloat(scan.next());
+			item_amount.put(food, 0);
+			item_pplWhoBought.put(food, 0);
+		}
+		
+		int num_of_people = Integer.parseInt(scan.next());
+		
+		
+		
+		for (int j=0; j<num_of_people; j++) {
+			String first_name = scan.next();
+			String last_name = scan.next();
+			int num_of_items = Integer.parseInt(scan.next());
+			
+			String foods[] = new String[num_of_items];
+			
+			int index = 0;
+			
+			for (int k = 0; k<num_of_items; k++) {
+				Integer amount = Integer.parseInt(scan.next());
+				
+				String food = scan.next();
+				if(!Arrays.asList(foods).contains(food)) {
+					foods[index] = food;
+					index++;
+				}
+				int total = ((int)item_amount.get(food)+amount);
+				item_amount.put(food, total);
+			}
+			for (int k = 0; k<index; k++) {
+				int total = ((int)item_pplWhoBought.get(foods[k])+1);
+				item_pplWhoBought.put(foods[k], total);
+				
+			}
+		}
+		
+		System.out.println("amoutn of each item: "+item_amount);
+		System.out.println("amoutn of ppl who bought each item: "+item_pplWhoBought);
+		
 		
 	}
 }
